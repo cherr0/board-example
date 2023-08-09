e07d9e9f266a25908cc5cdeb492ba93bff61075b
Peter Park <adsl1664@gmail.com>
2023-08-09 22:17:08 +0900

    add: mirage js 패키지 추가

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,5 @@
 const Config: Record<string, string> = {
-  SERVER_RPC_URL: process.env.NEXT_PUBLIC_SERVER_RPC_URL,
-  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+  API_BASE_URL: 'http://localhost:3000/api'
 } as const
 
 export default Config
