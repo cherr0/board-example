4c2fd6edefddf700fec0b6220ee477128010cb8d
Peter Park <adsl1664@gmail.com>
2023-08-11 23:53:45 +0900

    add: 버튼 컴포넌트 추가

---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+import { ButtonWrapper } from './style'
+import { ButtonProps } from './type'
+
+export const Button = ({
+  width = 'hug',
+  variant = 'contained',
+  size = 'md',
+  children,
+  disabled,
+  loading = false,
+  ...restProps
+}: ButtonProps) => {
+  const [isLoading, setIsLoading] = useState(loading)
+
+  useEffect(() => {
+    setIsLoading(loading)
+  }, [loading])
+
+  return (
+    <ButtonWrapper
+      {...restProps}
+      width={width}
+      variant={variant}
+      size={size}
+      disabled={disabled || isLoading}
+    >
+      {children}
+    </ButtonWrapper>
+  )
+}
---
+++ src/components/ui/button/style.ts
@@ -0,0 +1,91 @@
+import styled, { css } from 'styled-components'
+
+import { ButtonProps } from './type'
+
+import { noneDraggable, shadow, typography } from '~/styles/mixin'
+import { OptionalStyleRecord, StyleRecord } from '~/types/common'
+
+const variantStyle: StyleRecord<NonNullable<ButtonProps['variant']>> = {
+  contained: css`
+    color: var(--white);
+    background-color: var(--accent-button-default);
+
+    :hover {
+      background-color: var(--accent-button-hover);
+    }
+    :disabled {
+      background-color: var(--accent-button-disabled);
+    }
+    :focus {
+      background-color: var(--accent-button-focused);
+    }
+  `,
+  outlined: css`
+    color: var(--accent-button-default);
+    background-color: transparent;
+    border: 1px solid var(--accent-button-default);
+
+    :hover {
+      color: var(--accent-button-hover);
+      border: 1px solid var(--accent-button-hover);
+    }
+    :disabled {
+      color: var(--accent-button-disabled);
+      border: 1px solid var(--accent-button-disabled);
+    }
+    :focus {
+      color: var(--accent-button-focused);
+      border: 1px solid var(--accent-button-focused);
+    }
+  `
+}
+
+const sizeStyle: StyleRecord<ButtonProps['size']> = {
+  sm: css`
+    padding: 4px 10px;
+  `,
+  md: css`
+    padding: 6px 16px;
+  `,
+  lg: css`
+    padding: 8px 22px;
+  `
+}
+
+const widthStyle: OptionalStyleRecord<ButtonProps['width']> = {
+  fill: css`
+    width: 100%;
+  `,
+  hug: css`
+    width: fit-content;
+  `
+}
+
+export const ButtonWrapper = styled.button<ButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
+  gap: 8px;
+  border-radius: 8px;
+  border: 1px solid;
+  transition:
+    border,
+    color,
+    background-color,
+    0.3s ease;
+
+  ${noneDraggable};
+  ${shadow.sm};
+  ${typography.title};
+  ${({ variant }) => variantStyle[variant]};
+  ${({ size }) => sizeStyle[size]};
+  ${({ width, fixedWidth }) => {
+    if (width === 'fixed') {
+      return css`
+        width: ${fixedWidth}px;
+      `
+    }
+    return widthStyle[width]
+  }};
+`
